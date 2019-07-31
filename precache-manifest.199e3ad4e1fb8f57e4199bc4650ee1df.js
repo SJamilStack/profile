3818ee218bd1b56f744d30428c5f57205af7aa52
Shihab Jamil <shihabims@gmail.com>
2019-07-31 22:18:58 +0600

    Updates

---
+++ precache-manifest.199e3ad4e1fb8f57e4199bc4650ee1df.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7131fcde1474fe467fa74d08b60b8ec",
+    "url": "/SJamilStack/profile/index.html"
+  },
+  {
+    "revision": "ada29f054e8634fbacc9",
+    "url": "/SJamilStack/profile/static/css/2.4800e0c9.chunk.css"
+  },
+  {
+    "revision": "306bc59b3ec1fa8090f0",
+    "url": "/SJamilStack/profile/static/css/main.fce68853.chunk.css"
+  },
+  {
+    "revision": "ada29f054e8634fbacc9",
+    "url": "/SJamilStack/profile/static/js/2.d3bf3f46.chunk.js"
+  },
+  {
+    "revision": "306bc59b3ec1fa8090f0",
+    "url": "/SJamilStack/profile/static/js/main.3e5babd6.chunk.js"
+  },
+  {
+    "revision": "68b749a380b0bc606dfd",
+    "url": "/SJamilStack/profile/static/js/runtime~main.244d58b8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/SJamilStack/profile/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/SJamilStack/profile/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/SJamilStack/profile/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/SJamilStack/profile/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/SJamilStack/profile/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5c108329d53ad51463d9c05f07a5e73d",
+    "url": "/SJamilStack/profile/static/media/profile.5c108329.jpg"
+  }
+]);
